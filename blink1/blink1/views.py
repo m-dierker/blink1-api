bf2e361e1bf46ebad39c210c812ceeb6d148ed4a
Matthew <mvd7793@gmail.com>
2013-01-03 13:27:17 -0600

    Initial commit, works with red/green/blue/on/off

---
+++ blink1/blink1/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+import subprocess
+
+def process_command(request, cmd):
+    # Must be an absolute path (ex: no ~/bin/)
+    params = ['/Users/Matthew/bin/blink1-tool']
+
+    # Map these directly to --<string> with no interference. Ex: on should map to --on
+    direct_map = ['on', 'off', 'red', 'blue', 'green']
+
+    if cmd == 'on':
+        params.append('--on')
+    elif cmd in direct_map:
+        params.append('--' + cmd)
+    else:
+        return HttpResponse('Invalid Command')
+
+    print 'Executing ' + str(params)
+
+    # Process the command
+    output = subprocess.call(params)
+
+    return HttpResponse(output)
